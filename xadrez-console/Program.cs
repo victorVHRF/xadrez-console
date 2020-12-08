283ef0aae9f367a03905869cccff4c3361d980c8
Victor Hugo Rosa Fonseca <victor.vhrf@gmail.com>
2020-12-08 09:47:09 -0400

    criacao da classe posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -6,7 +7,10 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p;
+            p = new Posicao(3, 4);
+            Console.WriteLine("Posicao: " + p);
+            Console.ReadLine();
         }
     }
 }
